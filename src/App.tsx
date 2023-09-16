2341ff5c89862ea17385951b4a8e8295ca773125
FrankNZYMT <frank.adeleye@nzymtechnologies.com>
2023-09-16 12:24:00 +0100

    feat: update input field onchange function

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ const App: React.FC = () => {
   const { tasks, addTask, removeTask, toggleComplete, newTask, setNewTask } =
     useTask();
 
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    e.preventDefault();
+    setNewTask(e.target.value);
+  };
+
   return (
     <div className="App">
       <ul className="sidebar">
@@ -27,8 +32,8 @@ const App: React.FC = () => {
           <textarea
             placeholder="Add a new task"
             value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}></textarea>
-          <button onClick={addTask}>Add</button>
+            onChange={handleChange}></textarea>
+          <button onClick={addTask}>Create</button>
         </div>
       </div>
     </div>
